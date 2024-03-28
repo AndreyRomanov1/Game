0da32ef542b98035861b7838c2949e64bda53d00
Сурганов Денис <surganov.denis@gmail.com>
2024-03-28 16:02:00 +0500

    Поворот руки

---
+++ Assets/Pivot.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pivot : MonoBehaviour
+{
+    public float BaseAngle = 60f;
+    private void FixedUpdate()
+    {
+        var direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        direction.Normalize();
+        var rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + BaseAngle;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation);
+    }
+}
