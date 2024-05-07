aa4185e179f0fdd56176d42e5c4b28cf31fb6543
Сурганов Денис <surganov.denis@gmail.com>
2024-05-07 21:20:36 +0500

    Перезапуск игры после смерти

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -382,7 +382,7 @@ public class PlayerScript : MonoBehaviour, IDamageable
 
     private void Die()
     {
-        // CurrentGame.KillGame();
-        // Model.StartGame();
+        CurrentGame.KillGame();
+        Model.StartGame();
     }
 }
